838bf25e53b3ff7e63a24912c5819d465d97daff
Henry Vega <hp.vega@hotmail.com>
2017-11-26 09:57:43 -0500

    ticket

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -3,7 +3,6 @@
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-use app\widgets\Alert;
 use yii\helpers\Html;
 use app\assets\AppAsset;
 
@@ -13,9 +12,14 @@ AppAsset::register($this);
     <!DOCTYPE html>
     <html lang="<?php echo Yii::$app->language ?>">
     <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta http-equiv="Cache-control" content="public">
         <meta charset="<?php echo Yii::$app->charset ?>">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <meta name="robots" content="noindex, nofollow">
+        <link rel="shortcut icon" href="<?php echo Yii::$app->getHomeUrl(); ?>favicon.ico"
+              type="image/x-icon">
         <?php echo Html::csrfMetaTags() ?>
         <title><?php echo Html::encode($this->title) ?></title>
         <?php echo Html::cssFile('@web/css/ticket.min.css?v=' . filemtime(Yii::getAlias('@webroot/css/ticket.min.css'))) ?>
@@ -24,23 +28,20 @@ AppAsset::register($this);
     <body>
     <?php $this->beginBody() ?>
 
-    <?php echo Alert::widget() ?>
     <?php echo $content ?>
 
     <?php echo Html::jsFile('@web/js/all.js?v=' . filemtime(Yii::getAlias('@webroot/js/all.js'))) ?>
     <?php $this->endBody() ?>
-    <?php if (Yii::$app->user->isGuest) { ?>
-        <script type="text/javascript">
-            var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+    <?php if (!Yii::$app->user->isGuest) { ?>
+        <script type="text/javascript">var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
             (function () {
                 var s1 = document.createElement('script'), s0 = document.getElementsByTagName('script')[0]
                 s1.async = true
-                s1.src = 'https://embed.tawk.to/59dd7cb2c28eca75e4625528/default?v=1507494478'
+                s1.src = 'https://embed.tawk.to/59a4ec2e4fe3a1168eada2d2/default'
                 s1.charset = 'UTF-8'
                 s1.setAttribute('crossorigin', '*')
                 s0.parentNode.insertBefore(s1, s0)
-            })()
-        </script>
+            })()</script>
     <?php } ?>
     </body>
     </html>
