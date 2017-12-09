792211c1ec8b3c655108e94352d867cf2141053f
Henry Vega <hp.vega@hotmail.com>
2017-12-09 14:03:45 -0500

    Revert "Ajustes"
    This reverts commit 8c9df06d052f769dfa0e85be28e2a6a538490de2.

--- config/web.php
+++ config/web.php
@@ -24,8 +24,8 @@ $config = [
             ],
         ],
         'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'dca1t-E3Qw-YCl_sPf9FzyXsRlDrVvHy',
+            'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
